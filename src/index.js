50892a8ae3cb43f5be1976754d03aec5c8aed2a8
Lafa <01codeme@gmail.com>
2021-10-25 02:12:34 +0000

    improve all the files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 // Import all plugins
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from 'bootstrap'
+import * as bojqueryotstrap from 'jquery'
+
+
+//import mmenu
+// import * as mmenu from 'mmenu';
 
 // Or import only needed plugins
 // import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
@@ -112,51 +117,51 @@ $(function () {
     feather.replace();
 
     /* Client Logo slider with Adaptive height */
-    $('.client-logo-slider').slick({
-        dots: false,
-        infinite: true,
-        centerPadding: '60px',
-        speed: 300,
-        slidesToShow: 4,
-        adaptiveHeight: true,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        nextArrow: '<svg class="slick-next" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25"><g id="arrow-right-circle-fill" transform="translate(-10.125 -10.124)"><path id="Path_2" data-name="Path 2" d="M21.359,32.072A1.788,1.788,0,1,0,23.888,34.6L34.6,23.888a1.785,1.785,0,0,0,0-2.528L23.888,10.648a1.788,1.788,0,1,0-2.528,2.528l7.665,7.662H11.91a1.785,1.785,0,0,0,0,3.571H29.025l-7.667,7.664Z" fill="#1697d9" fill-rule="evenodd"/></g></svg>',
-        prevArrow: '<svg class="slick-prev" xmlns="http://www.w3.org/2000/svg" width="24.998" height="25" viewBox="0 0 24.998 25"><g id="arrow-left-circle-fill" transform="translate(0 -10.124)"><path id="Path_1" data-name="Path 1" d="M23.888,32.072A1.788,1.788,0,0,1,21.36,34.6L10.648,23.888a1.785,1.785,0,0,1,0-2.528L21.36,10.648a1.788,1.788,0,1,1,2.528,2.528l-7.667,7.662H33.336a1.785,1.785,0,0,1,0,3.571H16.222l7.667,7.664Z" transform="translate(-10.123 0)" fill="#1697d9" fill-rule="evenodd"/></g></svg> ',
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 769,
-                settings: {
-                    // rows: 2,
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 375,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
+    // $('.client-logo-slider').slick({
+    //     dots: false,
+    //     infinite: true,
+    //     centerPadding: '60px',
+    //     speed: 300,
+    //     slidesToShow: 4,
+    //     adaptiveHeight: true,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 5000,
+    //     nextArrow: '<svg class="slick-next" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25"><g id="arrow-right-circle-fill" transform="translate(-10.125 -10.124)"><path id="Path_2" data-name="Path 2" d="M21.359,32.072A1.788,1.788,0,1,0,23.888,34.6L34.6,23.888a1.785,1.785,0,0,0,0-2.528L23.888,10.648a1.788,1.788,0,1,0-2.528,2.528l7.665,7.662H11.91a1.785,1.785,0,0,0,0,3.571H29.025l-7.667,7.664Z" fill="#1697d9" fill-rule="evenodd"/></g></svg>',
+    //     prevArrow: '<svg class="slick-prev" xmlns="http://www.w3.org/2000/svg" width="24.998" height="25" viewBox="0 0 24.998 25"><g id="arrow-left-circle-fill" transform="translate(0 -10.124)"><path id="Path_1" data-name="Path 1" d="M23.888,32.072A1.788,1.788,0,0,1,21.36,34.6L10.648,23.888a1.785,1.785,0,0,1,0-2.528L21.36,10.648a1.788,1.788,0,1,1,2.528,2.528l-7.667,7.662H33.336a1.785,1.785,0,0,1,0,3.571H16.222l7.667,7.664Z" transform="translate(-10.123 0)" fill="#1697d9" fill-rule="evenodd"/></g></svg> ',
+    //     responsive: [
+    //         {
+    //             breakpoint: 1024,
+    //             settings: {
+    //                 slidesToShow: 4,
+    //                 slidesToScroll: 1,
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 992,
+    //             settings: {
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 769,
+    //             settings: {
+    //                 // rows: 2,
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 375,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1
+    //             }
+    //         }
 
-        ]
-    });
+    //     ]
+    // });
 
     /* Article slider with Adaptive height */
     $('.article-slider').slick({
@@ -219,7 +224,7 @@ $(function () {
             var items = slick.$slides;
             items.each(function (k, v) {
                 var text = $(this).find('h1').text();
-                $('.heading' + k).text(text);
+                $('.heading' + k).html("<span>"+ text + "</span>");
             });
 
         });
@@ -289,7 +294,71 @@ $(function () {
         
         }
         // end if innerWidth
-        }); 
-        // DOMContentLoaded  end
+        
+    }); 
+    // DOMContentLoaded  end
+
+    $('.full-slider button').on( "click", function (event) {
+
+        console.log("opended");
+
+        setTimeout(function(){ 
+            $(".ytp-large-play-button").click();
+        }, 20000);
+        
+    });
+
+    $('#exampleModalCenter .btn-close').on( "click",  function (event) {
+
+        console.log("closed");
+        $(".ytp-large-play-button").click();
+
+    });
+
+
+    /* Client Logo slider with Adaptive height */
+
+    let rows, slides;
+    function fixResponsiveClientSlider() {
+        var viewportWidth = $(window).width();
+        if( viewportWidth > 1024 ){
+            rows = 1;
+            slides = 4;
+          
+        } else if( viewportWidth > 768 ){
+            rows = 1;
+            slides = 3;
+            
+        } else {
+            rows = 2;
+            slides = 2;
+            
+        }
+    }
+    fixResponsiveClientSlider()
+    window.addEventListener('resize', fixResponsiveClientSlider);
+    
+    $('.client-logo-slider').slick({
+        dots: true,
+        nextArrow: '<svg class="slick-next" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25"><g id="arrow-right-circle-fill" transform="translate(-10.125 -10.124)"><path id="Path_2" data-name="Path 2" d="M21.359,32.072A1.788,1.788,0,1,0,23.888,34.6L34.6,23.888a1.785,1.785,0,0,0,0-2.528L23.888,10.648a1.788,1.788,0,1,0-2.528,2.528l7.665,7.662H11.91a1.785,1.785,0,0,0,0,3.571H29.025l-7.667,7.664Z" fill="#1697d9" fill-rule="evenodd"/></g></svg>',
+        prevArrow: '<svg class="slick-prev" xmlns="http://www.w3.org/2000/svg" width="24.998" height="25" viewBox="0 0 24.998 25"><g id="arrow-left-circle-fill" transform="translate(0 -10.124)"><path id="Path_1" data-name="Path 1" d="M23.888,32.072A1.788,1.788,0,0,1,21.36,34.6L10.648,23.888a1.785,1.785,0,0,1,0-2.528L21.36,10.648a1.788,1.788,0,1,1,2.528,2.528l-7.667,7.662H33.336a1.785,1.785,0,0,1,0,3.571H16.222l7.667,7.664Z" transform="translate(-10.123 0)" fill="#1697d9" fill-rule="evenodd"/></g></svg> ',
+        slidesToShow: slides,
+		slidesPerRow: 1,
+		rows: rows,
+		
+    });
+
+
+    $('#bs-example-navbar-collapse-1')
+    .on('shown.bs.collapse', function() {
+      $('#navbar-hamburger').addClass('hidden');
+      $('#navbar-close').removeClass('hidden');    
+    })
+    .on('hidden.bs.collapse', function() {
+      $('#navbar-hamburger').removeClass('hidden');
+      $('#navbar-close').addClass('hidden');        
+    });
+
+
 
 });
