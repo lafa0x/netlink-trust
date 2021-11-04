36680eceac92cb2e46037535d89ea9b787383dc6
Lafa <01codeme@gmail.com>
2021-11-04 15:23:33 +0000

    added new pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 // Import all plugins
-import * as bootstrap from 'bootstrap';
-// import * as jQuery from 'jquery';
+// window.$ = require('jquery');
+// window.jQuery = require('jquery');
+
+import * as bootstrap from '../node_modules/bootstrap';
+
+// import '../node_modules/bootstrap-select';
+// import '../node_modules/bootstrap-select/dist/js/i18n/defaults-am_ET';
+// import '../node_modules/slick-carousel';
+
+
 
 // window.$ = require('jquery');
 // window.jQuery = require('jquery');
@@ -102,8 +110,111 @@ document.addEventListener(
                 })
             });
 
+        } // end if innerWidth
+
+
+        // BS tabs hover (instead - hover write - click) - Contact Page
+        $('.nav-pills a').hover(function (e) {
+            e.preventDefault()
+            $(this).tab('show')
+        })
+
+        // To style all selects
+        // $('select').selectpicker();
+
+
+        var x, i, j, l, ll, selElmnt, a, b, c;
+
+        /*look for any elements with the class "custom-select":*/
+        x = document.getElementsByClassName("custom-select");
+        l = x.length;
+        for (i = 0; i < l; i++) {
+            selElmnt = x[i].getElementsByTagName("select")[0];
+            ll = selElmnt.length;
+
+            /*for each element, create a new DIV that will act as the selected item:*/
+            a = document.createElement("DIV");
+            a.setAttribute("class", "select-selected form-control");
+            a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+            x[i].appendChild(a);
+
+            /*for each element, create a new DIV that will contain the option list:*/
+            b = document.createElement("DIV");
+            b.setAttribute("class", "select-items select-hide");
+
+            for (j = 1; j < ll; j++) {
+                /*for each option in the original select element,
+                create a new DIV that will act as an option item:*/
+                c = document.createElement("DIV");
+                c.innerHTML = selElmnt.options[j].innerHTML;
+                c.addEventListener("click", function (e) {
+                    /*when an item is clicked, update the original select box,
+                    and the selected item:*/
+                    var y, i, k, s, h, sl, yl;
+                    s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                    sl = s.length;
+                    h = this.parentNode.previousSibling;
+                    for (i = 0; i < sl; i++) {
+                        if (s.options[i].innerHTML == this.innerHTML) {
+                            s.selectedIndex = i;
+                            h.innerHTML = this.innerHTML;
+                            y = this.parentNode.getElementsByClassName("same-as-selected");
+                            yl = y.length;
+                            for (k = 0; k < yl; k++) {
+                                y[k].removeAttribute("class");
+                            }
+                            this.setAttribute("class", "same-as-selected");
+                            break;
+                        }
+                    }
+                    h.click();
+                });
+                b.appendChild(c);
+            }
+
+            x[i].appendChild(b);
+            a.addEventListener("click", function (e) {
+
+                /*when the select box is clicked, close any other select boxes,
+                and open/close the current select box:*/
+                e.stopPropagation();
+                closeAllSelect(this);
+                this.nextSibling.classList.toggle("select-hide");
+                this.classList.toggle("select-arrow-active");
+
+            });
+
         }
-        // end if innerWidth
+
+        function closeAllSelect(elmnt) {
+            /*a function that will close all select boxes in the document,
+            except the current select box:*/
+            var x, y, i, xl, yl, arrNo = [];
+            x = document.getElementsByClassName("select-items");
+            y = document.getElementsByClassName("select-selected");
+            xl = x.length;
+            yl = y.length;
+
+            for (i = 0; i < yl; i++) {
+                if (elmnt == y[i]) {
+                    arrNo.push(i)
+                } else {
+                    y[i].classList.remove("select-arrow-active");
+                }
+            }
+
+            for (i = 0; i < xl; i++) {
+                if (arrNo.indexOf(i)) {
+                    x[i].classList.add("select-hide");
+                    x[i].parentElement.classList.add("added")
+                }
+            }
+
+        }
+
+        /*if the user clicks anywhere outside the select box,
+        then close all select boxes:*/
+        document.addEventListener("click", closeAllSelect);
 
     });
 
@@ -455,10 +566,6 @@ $(function () {
     //   $('#navbar-hamburger').removeClass('hidden');
     //   $('#navbar-close').addClass('hidden');        
     // });
-
-
-
-
 
 
 
